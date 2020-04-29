d757ff1493a20ae2a8537d56a5a5b076d6e74d3a
Ryo Yasumori <kylasakerskog26@gmail.com>
2020-04-29 23:20:05 +0900

    implement add and substract

--- 9cc.c
+++ 9cc.c
@@ -6,6 +6,8 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char *p = argv[1];
+    
     // intel記法の選択
     printf(".intel_syntax noprefix \n");
 
@@ -14,8 +16,31 @@ int main(int argc, char **argv) {
 
     printf("main:\n");
 
-    // 文字列から数値への変換
-    printf("  mov rax, %d\n", atoi(argv[1]));
+    /* 
+        strtol(const char *s, char **endptr, int base)
+          文字列sをlong型に変換し返却し，変換不可能な文字があった場合はendptrに格納
+            *s: 変換対象文字列
+            **endptr: 変換不可能な文字列へのポインタの格納先
+            base: 進数
+    */
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p){
+        if (*p == '+') {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p,10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p,10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    }
+    
     printf("  ret\n");
     return 0;
 }
